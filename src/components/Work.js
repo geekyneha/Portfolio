064ef6c4f73c5f0c9a2f2d21978b227240e45201
Neha <nehatualea@gmail.com>
2023-07-10 07:35:28 +0530

    fixed

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,7 +14,7 @@ const Work = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex flex-col lg:gap-y-12 mb-10 lg:mb-0 lg:static relative left-20"
+            className="flex-1 flex flex-col lg:gap-y-12 mb-10 lg:mb-0 lg:static relative left-18"
           >
             <div className="w-full flex flex-col lg:flex-row gap-10 justify-center items-center">
               {PROJECTS.map((project) => {
