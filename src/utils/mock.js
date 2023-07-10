cb5337439ac4c9b9390e7efd2e491c16096a7125
Neha <nehatualea@gmail.com>
2023-07-10 06:19:26 +0530

    Added:urls

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -13,27 +13,35 @@ export const  PROJECTS=[
    about:"The Gym Website is an interactive platform for a fitness center that demonstrates my expertise in web development, UX design, and the utilization of libraries. It incorporates a sticky header, user registration/login functionality, an engaging About Us section featuring a YouTube video, scrollable Training Programs, a dedicated Trainer section, Pricing details, and seamless integration with social media. This project serves as a testament to my skills in creating dynamic websites for fitness centers, offering a visually appealing and user-friendly experience to visitors.",
    catagory:"Fitness/Health & Wellness",
    src:image1,
+   githubLink:"https://github.com/geekyneha/gym-website",
+   deployedLink:"gym-clubb.vercel.app"
   },
   {
     id: uuidv4(),
     name:"Google Doc",
    about:"This project showcases a CSS-based implementation combined with a touch of React to enhance the functionality of a Google Docs-like application. By leveraging CSS styling and React components, this project offers an intuitive and visually appealing interface for creating and editing documents.",
-   catogory:" Document Collaboration/Office/Productivity",
-   src:image2
+   catagory:"Document Office",
+   src:image2,
+   githubLink:"https://github.com/geekyneha/google-doc/",
+   deployedLink:"google-document-clone.vercel.app"
   },
   {
     id: uuidv4(),
     name: "Kanban Board",
    about:": This project introduces a simple implementation of a Kanban Board, an effective tool for visualizing and managing work tasks. The Kanban Board utilizes Post-it notes to represent tasks and their statuses, providing an intuitive system for tracking progress.",
-   catagory:"Project Management/Productivity",
-   src:image3
+   catagory:"Project Managemen/Productivity",
+   src:image3,
+   githubLink:"https://github.com/shubham-tiwari171/kanban-project/tree/development#README.md",
+   deployedLink:"https://kanban-project-tm7o.vercel.app"
   },
   {
     id: uuidv4(),
     name: "Twitter clone",
    about:"This group project involved the successful creation of a Twitter clone, replicating key features of the popular microblogging platform. The team worked collaboratively to implement essential functionalities, including user authentication, protected routing for secure access, and a comprehensive tweet section allowing users to interact with posts through likes and dislikes.",
-   catogory:"Social Media",
-   src:image4
+   catagory:"Social Media",
+   src:image4,
+   githubLink:"https://github.com/shubham-tiwari171/function-up-tiwtter-clone/blob/development/README.md",
+   deployedLink:"https://legendary-crisp-0923a2.netlify.app"
   },
 
 ]
